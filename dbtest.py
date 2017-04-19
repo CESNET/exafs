de4dd5797ddbd7b35406e5e449bc743a8c29e3b2
Jiri Vrany <jiri.vrany@tul.cz>
2017-04-19 08:36:59 +0200

    load config from dbtool

--- dbtest.py
+++ dbtest.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import models
+import config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@127.0.0.1:3306/flowtest?host=127.0.0.1?port=3306'
+
+# Configurations
+if path.realpath(__file__) == '/home/albert/work/flowspec/www/flowapp.py':
+    app.config.from_object(config.DevelopmentConfig)
+else: 
+    app.config.from_object(config.ProductionConfig)
+    
 db = SQLAlchemy(app)
 
 
