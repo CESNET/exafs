f9d131279d52c91d3270b41df8b7d2d72d9b6c03
Jiri Vrany <jiri.vrany@tul.cz>
2017-07-14 11:02:37 +0200

    new version of db_init

--- db-init.py
+++ db-init.py
@@ -1,36 +1,42 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db
+from models import *
 
 import config
 from os import environ
 
-app = Flask(__name__)
+def create_app():
+    app = Flask('FlowSpecDB init')
+    # Configurations
+    try:
+        env = environ['USERNAME']
+    except KeyError as e:
+        env = 'Production'
+        
+    if env=='albert':
+        app.config.from_object(config.DevelopmentConfig)
+    else: 
+        app.config.from_object(config.ProductionConfig)
 
-
-# Configurations
-try:
-    env = environ['USERNAME']
-except KeyError as e:
-    env = 'Production'
+    db.init_app(app)
     
-if env=='albert':
-    app.config.from_object(config.DevelopmentConfig)
-else: 
-    app.config.from_object(config.ProductionConfig)
 
-db = SQLAlchemy(app)
+    with app.app_context():
+        print "#: cleaning database"
+        db.reflect()
+        db.drop_all()
+        print "#: creating tables"
+        db.create_all()   
 
-from models import *
+        users = [
+                {"name": "jiri.vrany@tul.cz", "role_id": 3, "org_id": 1},
+                {"name": "petr.adamec@tul.cz", "role_id": 3, "org_id": 1} 
+            ]
+        print "#: inserting users"
+        insert_users(users)     
 
-print "#: cleaning database"
-db.reflect()
-db.drop_all()
-print "#: creating tables"
-db.create_all()   
 
-users = [
-        {"name": "jiri.vrany@tul.cz", "role_id": 3, "org_id": 1},
-        {"name": "petr.adamec@tul.cz", "role_id": 3, "org_id": 1} 
-    ]
-print "#: inserting users"
-insert_users(users)         
+    return app
+
+if __name__ == '__main__':
+    create_app().app_context().push()    
