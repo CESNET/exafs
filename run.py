25c7a338f82232f0c73184125ad076099d566da6
Jiri Vrany <jiri.vrany@tul.cz>
2018-03-05 12:04:01 +0100

    debug start new app

--- run.py
+++ run.py
@@ -16,4 +16,5 @@ else:
 # init database object
 db.init_app(app)
 # run app
-app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
