143ecdecd901bab9080e3ee23583aa351a47344c
Jiri Vrany <jiri.vrany@tul.cz>
2019-09-16 13:30:47 +0200

    v 4.0.1 - with group ops in admin dashboard

--- flowapp/__init__.py
+++ flowapp/__init__.py
@@ -6,7 +6,7 @@ from flask_sso import SSO
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
-__version__ = '0.4.0'
+__version__ = '0.4.1    '
 
 app = Flask(__name__)
 
