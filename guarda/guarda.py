28a9242c9160e870e3cc1fa54dac6b437058d302
Jiri Vrany <jiri.vrany@tul.cz>
2020-12-14 16:52:02 +0100

    external config.py for guarda service

--- guarda/guarda.py
+++ guarda/guarda.py
@@ -2,8 +2,8 @@
 
 import requests
 import time
+import config
 
-# requests.get('https://127.0.0.1/announce_all')
 time.sleep(10)
 # replace URL 
-requests.get('https://exafs.cesnet.cz/rules/announce_all')
+requests.get(config.URL)
