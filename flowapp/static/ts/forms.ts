c4466265236de44f03ddf8a296760ea0c28038a7
Jakub Man <jakubman1@gmail.com>
2022-07-11 15:24:30 +0200

    Add function to dynamically hide elements on condition

--- flowapp/static/ts/forms.ts
+++ flowapp/static/ts/forms.ts
@@ -34,7 +34,11 @@ export function fillOrganization(ipInputId: string, maskInputId: string): void {
 }
 
 /***
- * Fill value from the
+ * Fill value from the selected organization range to appropriate fields.
+ * If a field is null, the value is skipped.
+ *
+ * @param {HTMLInputElement} ipField - A field to input the IP value to
+ * @param {HTMLInputElement} maskField - A field to input the mask value to
  */
 function fillOrgValue(ipField: HTMLInputElement | null, maskField: HTMLInputElement | null) {
     const org_select = document.getElementById('orgSelect') as HTMLInputElement;
@@ -83,5 +87,26 @@ export function changeAllCheckboxes(newValue: boolean, selector="input[type='che
     const inputs = document.querySelectorAll(selector) as NodeListOf<HTMLInputElement>;
     for (let input of inputs) {
         input.checked = newValue;
+    }
+}
+
+/***
+ * Add the 'd-none' CSS class to a specified element based on a condition
+ * that the sourceValue is not equal to the showValue. Otherwise, remove
+ * the 'd-none' CSS class.
+ *
+ * @param {any} sourceValue - Value of the element that specifies the condition to show the target element,
+ *                            e.g. value of the protocol select element
+ * @param {any} showValue   - Value to compare the condition element to. If the source value is equal to
+ *                            the show value, the target element is visible ('d-none' CSS class is removed)
+ * @param {string} targetID - ID of the element, that the 'd-none' class should be added to/removed from.
+ */
+export function hideFieldIfNot(sourceValue: any, showValue: any, targetID: string) {
+    const elem = document.getElementById(targetID);
+    if (sourceValue != showValue) {
+        elem?.classList.add('d-none');
+    }
+    else {
+        elem?.classList.remove('d-none');
     }
 }
