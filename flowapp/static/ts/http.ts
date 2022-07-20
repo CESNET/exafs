a94a745ec588c04154f320007050ed9a4841c609
Jakub Man <jakubman1@gmail.com>
2022-07-20 09:42:50 +0200

    Change the sendToBackend function to return promise directly instead of taking callbacks as parameters

--- flowapp/static/ts/http.ts
+++ flowapp/static/ts/http.ts
@@ -5,17 +5,11 @@
  * @param {FormData} data - data to send to the `url`.
  * @param {string} url - URL to send the data to.
  * @param {string} method - HTTP method to use. Valid values are "POST", "GET", "PUT", "PATCH", and "DELETE.
- * @param {(response: Response) => void} callbackSuccess - Function to call if the request was successful
- * @param {(error: string) => void} callbackFail - Function to call if the request was not successful
  * */
-export function sendToBackend(data: FormData, url: string, method: string, callbackSuccess: (response: Response) => void, callbackFail: (error: string) => void) {
+export function sendFormDataToBackend(data: FormData, url: string, method: string): Promise<Response> {
     const requestOpts: RequestInit = {
-        method: method, //"POST",
+        method: method.toUpperCase(), //"POST",
         body: data
     };
-    console.log('sending data:');
-    console.log(data);
-    fetch(url, requestOpts)
-        .then(response => callbackSuccess(response))
-        .catch(error => callbackFail(error));
+    return fetch(url, requestOpts)
 }
