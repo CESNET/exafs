11aba6db04894e22b6d9c55ec0d19f8c71729cc4
Jakub Man <jakubman1@gmail.com>
2022-07-20 09:41:22 +0200

    Add renderer functions to remove HTML elements

--- flowapp/static/ts/renderer.ts
+++ flowapp/static/ts/renderer.ts
@@ -1,4 +1,3 @@
-
 /***
  * Convert HTML formatted string to HTML element
  *
@@ -23,16 +22,58 @@ export function stringToHtml(str: string): HTMLCollection {
  */
 export function attachHtmlTo(html: HTMLElement | HTMLCollection, targetID: string) {
     const target = document.getElementById(targetID);
-    if (html instanceof HTMLElement) {
-        target?.appendChild(html);
+    if(target) {
+        attachHtmlToRef(html, target);
     }
-    else if(html instanceof HTMLCollection) {
+}
+
+/***
+ * Attach HTML to another element.
+ *
+ * @param {HTMLElement | HTMLCollection} html - HTML object to attach
+ * @param {string}                       ref  - HTML element to attach the new HTML to
+ */
+export function attachHtmlToRef(html: HTMLElement | HTMLCollection, ref: HTMLElement) {
+    if (html instanceof HTMLElement) {
+        ref.appendChild(html);
+    } else if (html instanceof HTMLCollection) {
         let arr = Array.from(html);
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < arr.length; i++) {
             const item = arr[i];
-            if (item) {
-                target?.appendChild(item);
-            }
+            ref.appendChild(item);
         }
     }
+}
+
+/***
+ * Convert string into HTML and attach it as a child to another element
+ * identified by CSS ID.
+ *
+ * @param {string} str - Valid HTML in a string
+ * @param {string} parentID - ID of the element to attach the new HTML to
+ */
+export function createChild(str: string, parentID: string) {
+    attachHtmlTo(stringToHtml(str), parentID);
+}
+
+/***
+ * Remove all children of a given element specified by a CSS id
+ *
+ * @param {string} id: CSS ID of an element, whose children should be removed.
+ * */
+export function clearAllChildren(id: string) {
+    const elem = document.getElementById(id);
+    if (elem) {
+        elem.innerHTML = '';
+    }
+}
+
+/***
+ * Remove element from page. Does not work for top-level elements that do not
+ * have parent elements.
+ *
+ * @param {HTMLElement} ref - Element to remove
+ */
+export function removeElement(ref: HTMLElement) {
+    ref.parentElement?.removeChild(ref);
 }
