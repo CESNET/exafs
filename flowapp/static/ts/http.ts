4a63f61d7a35f374655a072eb15c173b1090b7bb
Jakub Man <jakubman1@gmail.com>
2022-07-20 09:43:09 +0200

    Add function for sending JSON data to backend

--- flowapp/static/ts/http.ts
+++ flowapp/static/ts/http.ts
@@ -13,3 +13,32 @@ export function sendFormDataToBackend(data: FormData, url: string, method: strin
     };
     return fetch(url, requestOpts)
 }
+
+/***
+ * Send any JSON object to a specified url. If the request is successful,
+ * the `callbackSuccess` callback is called, otherwise the `callbackFail` is called.
+ *
+ * @param {FormData} data - data to send to the `url`.
+ * @param {string} url - URL to send the data to.
+ * @param {string} method - HTTP method to use. Valid values are "POST", "GET", "PUT", "PATCH", and "DELETE.
+ * @param {string} csrfToken - CSRF token from WTForms, will be included in the `X-CSRFToken` HTTP header
+ * */
+export function sendJsonToBackend(
+    data: object,
+    url: string,
+    method: string,
+    csrfToken: string): Promise<Response> {
+
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('X-CSRFToken', csrfToken);
+    requestHeaders.set('Accept', 'application/json, text/plain, */*');
+    requestHeaders.set('Content-Type', 'application/json');
+
+    const requestOpts: RequestInit = {
+        method: method,
+        headers: requestHeaders,
+        body: JSON.stringify(data)
+    };
+
+    return fetch(url, requestOpts);
+}
