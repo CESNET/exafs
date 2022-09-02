a9920631947075cc8a7efb27a440fcf660b5ceea
Jiri Vrany <jiri.vrany@tul.cz>
2022-09-02 10:39:28 +0200

    V 0.6.2 - exaapi configuration moved to config file

---
+++ exaapi/config.example.py
@@ -0,0 +1,8 @@
+"""
+Example of configuration file
+
+Add your log settings and rename to config.py
+"""
+
+LOG_FILE = '/var/log/exafs/exa_api.log'
+LOG_FORMAT = '%(asctime)s:  %(message)s'
--- exaapi/exa_api.py
+++ exaapi/exa_api.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 """
-This module is used as simple echo
-each command received in the POST request is send to stdout
+This module is process for ExaBGP
+https://github.com/Exa-Networks/exabgp/wiki/Controlling-ExaBGP-:-possible-options-for-process
+
+Each command received in the POST request is send to stdout and captured by ExaBGP.
 """
 import logging
 from flask import Flask, request
 from sys import stdout
 
+import config
+
 app = Flask(__name__)
 
 #logovani
 logger = logging.getLogger(__name__)
-f_format = logging.Formatter('%(asctime)s:  %(message)s')
-f_handler = logging.FileHandler('/var/log/exafs/exa_api.log')
+f_format = logging.Formatter(config.LOG_FORMAT)
+f_handler = logging.FileHandler(config.LOG_FILE)
 f_handler.setFormatter(f_format)
 logger.setLevel(logging.INFO)
 logger.addHandler(f_handler)
