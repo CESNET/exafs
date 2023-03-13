CESNET/exafs

commit 0e45cd04c07039b9eebfd7a64993db9390daef11

v 0.7.0 - two options for ExaAPI

flowapp/output.py
@@ -4,6 +4,7 @@ Module for message announcing and logging
 from datetime import datetime
 
 import requests
+import pika
 from flask import current_app, session
 
 from flowapp import db, messages
@@ -14,20 +15,52 @@ RULE_TYPES = {'RTBH': 1, 'IPv4': 4, 'IPv6': 6}
 
 def announce_route(route):
     """
-    Announce route to ExaAPI
+    Dispatch route to ExaBGP API 
+    API must be set in app config.py 
+    defaults to HTTP API
+    """
+    if current_app.config['EXA_API'] == 'RABBIT':
+        announce_to_rabbitmq(route)
+    else:
+        announce_to_http(route)
 
-    @TODO take the request away, use some kind of messaging (maybe celery?)
+def announce_to_http(route):
+    """
+    Announce route to ExaBGP HTTP API process 
     """
     if not current_app.config['TESTING']:
         try:
-            resp = requests.post('http://localhost:5000/', data={'command': route})
+            resp = requests.post(current_app.config['EXA_API_URL'], data={'command': route})
             resp.raise_for_status()
         except requests.exceptions.HTTPError as err:
             print("ExaAPI HTTP Error: ", err)
         except requests.exceptions.RequestException as ce:
             print("Connection to ExaAPI failed: ", ce)
+    else:
+        print("Testing:", route)        
             
+def announce_to_rabbitmq(route):
+    """
+    Announce rout to ExaBGP RabbitMQ API process
+    """
+    if not current_app.config['TESTING']:
+        user = current_app.config['EXA_API_RABBIT_USER']
+        passwd = current_app.config['EXA_API_RABBIT_PASS']
+        queue = current_app.config['EXA_API_RABBIT_QUEUE']
+        credentials = pika.PlainCredentials(user, passwd)
+        parameters = pika.ConnectionParameters(current_app.config['EXA_API_RABBIT_HOST'],
+                                            current_app.config['EXA_API_RABBIT_PORT'],
+                                            '/',
+                                            credentials)
 
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+        channel.queue_declare(queue=queue)
+        channel.basic_publish(exchange='',
+                      routing_key=queue,
+                      body=route)
+    else:
+        print("Testing:", route)        
 
 def log_route(user_id, route_model, rule_type, author):
     """

exaapi/exa_api_http.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 """
+ExaBGP HTTP API process
 This module is process for ExaBGP
 https://github.com/Exa-Networks/exabgp/wiki/Controlling-ExaBGP-:-possible-options-for-process
 
 Each command received in the POST request is send to stdout and captured by ExaBGP.
 """
-import logging
+
 from flask import Flask, request, abort
 from sys import stdout
 
-import config
+import exa_api_logger
 
 app = Flask(__name__)
 
-#logovani
-logger = logging.getLogger(__name__)
-f_format = logging.Formatter(config.LOG_FORMAT)
-f_handler = logging.FileHandler(config.LOG_FILE)
-f_handler.setFormatter(f_format)
-logger.setLevel(logging.INFO)
-logger.addHandler(f_handler)
+logger = exa_api_logger.create()
 
 
 @app.route('/', methods=['POST'])
@@ -28,7 +23,6 @@ def command():
     logger.info(cmd)
     stdout.write('%s\n' % cmd)
     stdout.flush()
-    
 
     return '%s\n' % cmd
 

exaapi/exa_api_logger.py
@@ -1,0 +1,11 @@
+import logging
+import config
+
+def create():
+    logger = logging.getLogger(__name__)
+    f_format = logging.Formatter(config.LOG_FORMAT)
+    f_handler = logging.FileHandler(config.LOG_FILE)
+    f_handler.setFormatter(f_format)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(f_handler)
+    return logger

exaapi/exa_api_rabbit.py
@@ -1,0 +1,50 @@
+#!/usr/bin/env python
+"""
+ExaBGP RabbitMQ API process
+This module is process for ExaBGP
+https://github.com/Exa-Networks/exabgp/wiki/Controlling-ExaBGP-:-possible-options-for-process
+
+Each command received from the queue is send to stdout and captured by ExaBGP.
+"""
+
+import pika, sys, os
+
+import config
+import exa_api_logger
+
+logger = exa_api_logger.create()
+
+def main():
+    user = config.EXA_API_RABBIT_USER
+    passwd = config.EXA_API_RABBIT_PASS
+    queue = config.EXA_API_RABBIT_QUEUE
+    credentials = pika.PlainCredentials(user, passwd)
+    parameters = pika.ConnectionParameters(config.EXA_API_RABBIT_HOST,
+                                        config.EXA_API_RABBIT_PORT,
+                                        '/',
+                                        credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue)                                        
+
+    def callback(ch, method, properties, body):
+        body = body.decode("utf-8") 
+        logger.info(body)
+        sys.stdout.write('%s\n' % body)
+        sys.stdout.flush()
+
+    channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nInterrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)

flowapp/__init__.py
@@ -6,7 +6,7 @@ from flask_sso import SSO
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
-__version__ = '0.6.2'
+__version__ = '0.7.0'
 
 app = Flask(__name__)
 

flowapp/tests/conftest.py
@@ -54,6 +54,8 @@ def app(request):
     """
 
     _app.config.update(
+        EXA_API = 'HTTP',
+        EXA_API_URL = 'http://localhost:5000/',
         TESTING=True,
         SQLALCHEMY_DATABASE_URI=TEST_DATABASE_URI,
         SQLALCHEMY_TRACK_MODIFICATIONS=False,

commit 23af19b436eed71d8296246e4e5d07e155f58291

fix crash when EXA_API key is not set in config
The GUI would crash on KeyError when trying to add a new rule,
unless the 'EXA_API' key was set in the config. Since the key
is not in the example config, just renaming the example config
would lead to the GUI not working.

This commit fixes the issue.

Fixes: 0e45cd0 ("v 0.7.0 - two options for ExaAPI")

flowapp/output.py
@@ -19,7 +19,7 @@ def announce_route(route):
     API must be set in app config.py 
     defaults to HTTP API
     """
-    if current_app.config['EXA_API'] == 'RABBIT':
+    if current_app.config.get('EXA_API') == 'RABBIT':
         announce_to_rabbitmq(route)
     else:
         announce_to_http(route)